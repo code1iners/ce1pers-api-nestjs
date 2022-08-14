219ba1d2cfea757e61e995f74b733ca309be7726
__coma__ <codeliner96@gmail.com>
2022-08-15 00:46:13 +0900

    Added schema validator.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,29 @@ import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { MembersModule } from './members/members.module';
-import { CoreModule } from './core/core.module';
-import { RepositoriesModule } from './repositories/repositories.module';
+import * as Joi from 'joi';
+import { MembersModule } from '@/members/members.module';
+import { CoreModule } from '@/core/core.module';
+import { RepositoriesModule } from '@/repositories/repositories.module';
+import { EnvKeys } from '@/core/constants/env-keys';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === EnvKeys.Production
+          ? EnvKeys.ProductionEnvFile
+          : EnvKeys.DevelopmentEnvFile,
+      ignoreEnvFile: process.env.NODE_ENV === EnvKeys.Production,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid(EnvKeys.Production, EnvKeys.Development)
+          .required(),
+        PORT: Joi.number().required(),
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
---
+++ src/core/constants/env-keys.ts
@@ -0,0 +1,6 @@
+export const EnvKeys = {
+  Development: 'development',
+  Production: 'production',
+  DevelopmentEnvFile: '.env.development',
+  ProductionEnvFile: '.env.production',
+} as const;
